c244697a8e6f5666512009b80a17c1a8200159af
katelynpeterson <33292506+katelynpeterson@users.noreply.github.com>
2018-11-09 08:46:22 -0700

    Add project files.

---
+++ TestOOUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ooui;
+using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace TestOOUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forms.Init();
+            UI.Port = 12345;
+            UI.Host = "localhost";
+            UI.Publish("/", new Page1() { }.GetOouiElement());
+            Process.Start("explorer", $"http://{UI.Host}:{UI.Port}");
+            Console.ReadLine();
+        }
+    }
+}
